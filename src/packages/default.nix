85f3f87d9bc1906911b797716ec4a6d751505f6e
Gaël Reyrol <me@gaelreyrol.dev>
2023-12-19 14:10:57 +0100

    chore(packages): add comment for tracetest

--- src/packages/default.nix
+++ src/packages/default.nix
@@ -5,5 +5,6 @@ prev:
 
 {
   otel-collector-builder = prev.callPackage ./otel-collector-builder.nix { };
+  # Remove when merged: https://github.com/NixOS/nixpkgs/pull/275406
   tracetest = prev.callPackage ./tracetest.nix { };
 }
